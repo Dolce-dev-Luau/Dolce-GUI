34a56a2161319deef7ab9993c1ab9f45d44227c5
Hoangthieen <hoanghaithien1508game@gmail.com>
2026-07-18 12:50:59 +0700

    Create init.lua

---
+++ src/Elements/init.lua
@@ -0,0 +1,10 @@
+-- Elements - Export all UI elements
+
+return {
+    require(script.Button),
+    require(script.Toggle),
+    require(script.Slider),
+    require(script.Dropdown),
+    require(script.Input),
+    require(script.Paragraph),
+}
